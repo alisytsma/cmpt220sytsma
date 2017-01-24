9eabd58de3e6b44bb5f90f1416f63a7a05cdff34
alisytsma <alissa.sytsma1@marist.edu>
2017-01-24 14:21:33 -0500

    lab 1

---
+++ labs/1/Problem3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Problem3 {
+	  
+  public static void main(String[] args){
+		
+    Scanner input = new Scanner(System.in);
+    //prompt the user to enter their numbers
+    System.out.print("Enter subtotal and gratuity rate: ");
+			
+    double subtotal = input.nextDouble();
+    double gratuity = input.nextDouble();
+    
+    //determine gratuity
+    double grat = subtotal * (gratuity / 100);
+    //add it to total
+    double total = grat + subtotal;
+		  
+    //print out gratuity and total
+    System.out.println("The gratuity is $" + grat + " and the total is $" + total);
+	   
+	}
+
+}
